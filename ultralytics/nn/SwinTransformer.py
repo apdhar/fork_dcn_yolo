fc95ca0097996367be7f4d75d3e7b9c42fa9f1c1
apdhar <148682331+apdhar@users.noreply.github.com>
2025-01-08 10:03:34 +0600

    Update SwinTransformer.py

--- ultralytics/nn/SwinTransformer.py
+++ ultralytics/nn/SwinTransformer.py
@@ -3,6 +3,15 @@ import torch.nn as nn
 import math
 import numpy as np
 
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+    
 class Conv(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
