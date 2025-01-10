9a6ff557fc9b2c2755b562bd8903cc8e8c9b4bca
apdhar <148682331+apdhar@users.noreply.github.com>
2025-01-10 15:02:42 +0600

    Update simAM.py

--- ultralytics/nn/simAM.py
+++ ultralytics/nn/simAM.py
@@ -1,6 +1,13 @@
 import torch
 import torch.nn as nn
 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
 
 class SimAM(torch.nn.Module):
     def __init__(self, channels = None,out_channels = None, e_lambda = 1e-4):
