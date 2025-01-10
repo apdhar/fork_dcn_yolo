d71dbcff93ae8e75bba2c09c3b25bf5417fc1ccd
apdhar <148682331+apdhar@users.noreply.github.com>
2025-01-10 15:03:35 +0600

    Update CoordConv.py

--- ultralytics/nn/CoordConv.py
+++ ultralytics/nn/CoordConv.py
@@ -1,6 +1,14 @@
 import torch
 import torch.nn as nn
 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
 class CoordConv(nn.Module):
     """CoordConv layer: Adds coordinate channels to the input tensor before applying a standard convolution."""
 
